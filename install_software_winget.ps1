23890c32ec577f5b6094f8f207e08267e69a1952
Andi <114431295+AndiVogt@users.noreply.github.com>
2024-07-02 09:34:02 +0200

    Create install_software_winget.ps1

---
+++ install_software_winget.ps1
@@ -0,0 +1,22 @@
+# install_software_winget.ps1
+winget install --id=Brave.Brave -e -h
+winget install --id=Valve.Steam -e -h
+winget install --id=Discord.Discord -e -h
+winget install --id=Microsoft.VisualStudioCode -e -h
+winget install --id=Mozilla.Firefox -e -h
+winget install --id=Obsidian.Obsidian -e -h
+winget install --id=OpenVPNTechnologies.OpenVPN -e -h
+winget install --id=PDF24.pdf24Creator -e -h
+winget install --id=Spotify.Spotify -e -h
+winget install --id=PuTTY.PuTTY -e -h
+winget install --id=Mozilla.Thunderbird -e -h
+winget install --id=Zotero.Zotero -e -h
+winget install --id=7zip.7zip -e -h
+winget install --id=Figma.Figma -e -h
+winget install --id=Git.Git -e -h
+winget install --id=Google.Drive -e -h
+winget install --id=OpenJS.Nodejs.LTS -e -h
+winget install --id=Python.Python.3 -e -h
+winget install --id=WhatsApp.WhatsApp -e -h
+winget install --id=Docker.DockerDesktop -e -h
+winget install --id=Microsoft.WindowsSubsystemForLinux -e -h
