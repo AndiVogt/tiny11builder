64325a7ec0af6cfbd02cb28f264f50ac6868d0d5
Andi <114431295+AndiVogt@users.noreply.github.com>
2024-10-13 11:01:11 +0200

    Update install_software_winget.ps1

--- install_software_winget.ps1
+++ install_software_winget.ps1
@@ -1,22 +1,35 @@
 # install_software_winget.ps1
 winget install --id=Brave.Brave -e -h
+winget install --id=Mozilla.Firefox -e -h
+winget install --id=Mozilla.Thunderbird -e -h
 winget install --id=Valve.Steam -e -h
 winget install --id=Discord.Discord -e -h
-winget install --id=Microsoft.VisualStudioCode -e -h
-winget install --id=Mozilla.Firefox -e -h
 winget install --id=Obsidian.Obsidian -e -h
-winget install --id=OpenVPNTechnologies.OpenVPN -e -h
 winget install --id=PDF24.pdf24Creator -e -h
 winget install --id=Spotify.Spotify -e -h
 winget install --id=PuTTY.PuTTY -e -h
-winget install --id=Mozilla.Thunderbird -e -h
+winget install --id=WinSCP.WinSCP -e -h
+winget install --id=OpenVPNTechnologies.OpenVPN -e -h
+winget install --id=KeePassXCTeam.KeePassXC -e -h
 winget install --id=Zotero.Zotero -e -h
 winget install --id=7zip.7zip -e -h
 winget install --id=Figma.Figma -e -h
-winget install --id=Git.Git -e -h
-winget install --id=Google.Drive -e -h
-winget install --id=OpenJS.Nodejs.LTS -e -h
+winget install --id=AppWork.JDownloader2 -e -h
+winget install --id=NVIDIA.GeForceGameReadyDriver -e -h
+winget install --id=NVIDIA.CUDA -e -h
+winget install --id=CPUID.HWMonitor -e -h
+
+# programming
 winget install --id=Python.Python.3 -e -h
-winget install --id=WhatsApp.WhatsApp -e -h
+winget install --id=OpenJS.Nodejs.LTS -e -h
+winget install --id=GoLang.Go -e -h
+winget install --id=Git.Git -e -h
 winget install --id=Docker.DockerDesktop -e -h
+winget install --id=ElementLabs.LMStudio -e -h
+
+winget install --id=Google.Drive -e -h
+winget install --id=WhatsApp.WhatsApp -e -h
+winget install --id=Microsoft.VisualStudioCode -e -h
 winget install --id=Microsoft.WindowsSubsystemForLinux -e -h
+winget install --id=Microsoft.OfficeDeploymentTool -e -h
+
